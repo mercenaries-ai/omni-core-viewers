22430d1f9cae394758e1cb87a9266dd53ac460d9
nieny225 <nieny225@gmail.com>
2023-10-11 16:51:44 +0800

    adding copyright to core extension

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2023 MERCENARIES.AI PTE. LTD.
+ * All rights reserved.
+ */
+
 import Handlebars, { HelperDelegate } from 'handlebars';
 import Alpine from 'alpinejs';
 import {OmniSDKClient} from 'omni-sdk';
